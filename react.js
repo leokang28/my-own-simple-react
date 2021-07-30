ff398933967fe7a4454390eb5b5d0a63db6d9caf
ksleo <leokang2828@gmail.com>
2021-07-30 14:03:45 +0800

    feat: add fiber

--- react.js
+++ react.js
@@ -21,19 +21,26 @@ function createTextElement(text) {
 }
 
 function render(element, container) {
-  const dom =
-    element.type === 'TEXT_ELEMENT'
-    ? document.createTextNode('')
-    : document.createElement(element.type);
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+function createDom(fiber) {
+  const dom = 
+    fiber.type === 'TEXT_ELEMENT'
+      ? document.createTextNode('')
+      : document.createElement(fiber.type);
 
   const isProperty = key => key !== 'children';
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProperty)
-    .forEach(name => {
-      dom[name] = element.props[name]
-    });
-  element.props.children.forEach(child => render(child, dom));
-  container.appendChild(dom);
+    .forEach(name => dom[name] = fiber.props[name]);
+
+  return dom;
 }
 
 let nextUnitOfWork = null;
@@ -49,8 +56,48 @@ function workLoop(deadline) {
 
 requestIdleCallback(workLoop);
 
-function performUnitOfWork(nextUnitOfWork) {
-  
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber = fiber;
+  while(nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 }
 
 const MyOwnReact = {
@@ -65,7 +112,7 @@ const element = (
     <h2 style="text-align:right">from MyOwnReact</h2>
   </div>
 );
+
 const container = document.getElementById("root")
 MyOwnReact.render(element, container)
-
 
