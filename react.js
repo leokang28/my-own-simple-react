b2e674d45d336f9324218b60038c6b9cfc2151f8
ksleo <leokang2828@gmail.com>
2021-07-30 13:34:11 +0800

    feat: createElement && render functions

---
+++ react.js
@@ -0,0 +1,54 @@
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map(child =>
+        typeof child === "object" ? child : createTextElement(child)
+      )
+    }
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: []
+    }
+  };
+}
+
+function render(element, container) {
+  const dom =
+    element.type === 'TEXT_ELEMENT'
+    ? document.createTextNode('')
+    : document.createElement(element.type);
+
+  const isProperty = key => key !== 'children';
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name]
+    });
+  element.props.children.forEach(child => render(child, dom));
+  container.appendChild(dom);
+}
+
+const MyOwnReact = {
+  createElement,
+  render,
+};
+
+/** @jsx MyOwnReact.createElement */
+const element = (
+  <div style="background: salmon">
+    <h1>Hello World</h1>
+    <h2 style="text-align:right">from MyOwnReact</h2>
+  </div>
+);
+const container = document.getElementById("root")
+MyOwnReact.render(element, container)
+
+
